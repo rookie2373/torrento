5683bd6641d58dc29e4c9bb31b3799177b2f593a
rushikesh <r4002001005025k@gmail.com>
2021-10-16 18:09:50 +0530

    added client configuration

---
+++ config.py
@@ -0,0 +1,7 @@
+import math
+"global"
+CONFIG = {
+    'peer_id': b'QQ-0000-000000000000', # unique id for client
+     'block_length': math.pow(2,14), # portion of data that a client request from the peer
+     'max_peers': 10 # numwant
+}
