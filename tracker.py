aa527a1fc5c52f27b5413bddf7571e012cbbe0ef
sanket chaudhari <sanketchaudhari.in20@gmail.com>
2021-11-11 16:41:14 +0000

    added method for udp tracker.py

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@
 import struct
 import requests
 import bencodepy
-
+import socket
 # Configuration file
 from config import CONFIG
 
@@ -14,22 +14,107 @@ debug = False
 # Connecting to tracker
 
 # Function to send announce request to the tracker
-def clientRequest(metaInfo):
-    announce = metaInfo["announce"]
-    response = requests.get(announce, {
-        'info_hash':metaInfo['info_hash'],
-        'peer_id': CONFIG['peer_id'],
-        'port': 6881,  # range (6881,6889)
-        'uploaded': '0',  # total amount of upload
-        'downloaded': '0',  # total amount of downloud
-        'left': str(metaInfo['info']['length']),
-        # 'numwant': CONFIG['max_peers']
-    })
 
-    # if(debug):
-    #     print(response,response.text)
+# sending the anncounce request using http get request and udp
+def client_request(torrent, metainfo, announce):
+    str_ann = str(announce)
+    protocol = str_ann[2:5]
+    url = ''
+    flag = 0
+    port = '' # to extract the port from the announce
+    for x in str_ann:
+        if x == '/' and flag != 2:
+            flag = 1
+        elif x == ':' and flag==1:
+            flag = 2
+        elif flag == 2 and x.isdigit():
+            port += x
+        elif flag == 1:
+            url += x
 
-    trackerResponse(metaInfo, response)
+
+    if protocol == 'udp':
+
+        S = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        host = socket.gethostbyname(url)
+        port = int(port) # extracted port
+        connection_id = 0x41727101980
+        action =0
+        transaction_id = 5400 # random
+        connect_request = struct.pack(">QLL",connection_id,action,transaction_id)
+        S.sendto(connect_request,(host,port))
+        resp1 = S.recv(16)
+        action, transaction_Id, connection_id = struct.unpack(">LLQ",resp1)
+        info_hash= metainfo['info_hash']
+        peer_id =CONFIG['peer_id']
+        port_p =6883  # range (6881,6889)
+        uploaded=0  # total amount of upload
+        downloaded= 0  # total amount of downloud
+        left =int(metainfo['info']['length'])
+        event = 2
+        ip = 0
+        key = 0
+        num_want = 10
+        action = 1
+        send_data = struct.pack(">QLL20s20sQQQLLLLH", connection_id, action,transaction_Id,info_hash, peer_id, downloaded,left,uploaded,event,ip,key,num_want,port_p)
+        S.sendto(send_data, (host, port))
+        resp2 = S.recv(1024)
+        resp_dict = {}
+        action, transaction_id, intervel, leechers, seeders = struct.unpack("!LLLLL", resp2[:20])
+        #print(action,transaction_id,intervel,leechers,seeders)
+        udp_trackers_resp(torrent,resp2)
+        resp_dict['action'] = action
+        resp_dict['transaction_id'] = transaction_id
+        resp_dict['interval'] = intervel
+        resp_dict['leechers'] = leechers
+        resp_dict['seeders'] = seeders
+
+    else:
+        resp = requests.get(announce, {
+            'info_hash': metainfo['info_hash'],
+            'peer_id': CONFIG['peer_id'],
+            'port': 6883,  # range (6881,6889)
+            'uploaded': '0',  # total amount of upload
+            'downloaded': '0',  # total amount of downloud
+            'left': str(metainfo['info']['length']),
+        })
+        trackers_response(torrent, resp)
+    return
+
+
+def create_ip(ip_tuple):
+    s=''
+    for i in ip_tuple:
+        s+=str(i)
+        s+='.'
+    return s[:len(s)-1]
+
+def create_port(port_tuple):
+    s=''
+    for i in port_tuple:
+        s+=str(i)
+    return int(s)
+
+def udp_trackers_resp(torrent,resp_):
+    resp = resp_[20:]
+    peer_list = []
+    i = 0
+    while i < len(resp):
+        peer_dict ={}
+        _ip = struct.unpack("!BBBB",resp[i:i+4])
+        _port = struct.unpack("!H",resp[i+4:i+6])
+        peer_dict['ip'] = create_ip(_ip)
+        peer_dict['port'] = create_port(_port)
+        peer_list.append(peer_dict)
+        i += 6
+
+
+    for peer_d in peer_list:
+
+        if peer_d['ip'] and peer_d['port'] > 0:
+            torrent.make_peerlist(peer_d)
+
+
 
 # Function to record tracker's response
 def trackerResponse(torrent, http_resp):
