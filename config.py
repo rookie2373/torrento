9c24324bcb1f6bd6b8cdf73113f1ac2222485604
rushikesh <r4002001005025k@gmail.com>
2021-11-13 22:01:49 +0530

    solved typeError integer

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Client Configuration file
 
 import math
-"global"
+# "global"
 
 CONFIG = {
     'peer_id': b'QQ-0000-000000000000', # unique id for client
-    'block_length': math.pow(2,14), # portion of data that a client request from the peer
+    'block_length': int(math.pow(2,14)), # portion of data that a client request from the peer
     'max_peers': 10 # numwant
 }
